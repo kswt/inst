27ae2b2ed3bd6a917fece687ebfbae95125a8279
Your Name <you@example.com>
2017-05-09 01:17:03 +0300

    Unix_66 v0.2

--- unix_66.sh
+++ unix_66.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-
-
 read -p "Please, enter the number: " n
 
 users_list=$(users | tr ' ' '\n' | sort | uniq)
-average_process=0
-nusers=0
 for un in $users_list; do
 	let average_process+=$(ps -u "$un" | wc -l)
 	let nusers++
@@ -14,12 +10,9 @@ let average_process/=nusers
 
 for un in $users_list; do
     usrn=$(ps -u "$un" | wc -l)
-    echo "Average processes per user is $average_process" | write "$un"
+    echo "Average number of processes per user is $average_process" | write "$un"
     if [ $(expr $average_process - $usrn) -gt $n ];
     then 
-        echo "You have $usrn process(es)."\
-             "It's greater than average + $n." | write "$un"
+        echo "You have $usrn process(es), it's greater than average + $n." | write "$un"
     fi
 done
-
-exit 0
